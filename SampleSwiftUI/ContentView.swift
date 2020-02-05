d2b5be3db856dc1babdb991a47a2c78391e53a15
lordkevinmo <moise.agbenya@gmail.com>
2020-02-05 20:44:47 +0100

    Initial Commit

---
+++ SampleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleSwiftUI
+//
+//  Created by Moïse AGBENYA on 05/02/2020.
+//  Copyright © 2020 Moïse AGBENYA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
